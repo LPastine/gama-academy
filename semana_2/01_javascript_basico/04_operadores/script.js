423e1b8966aca4a024c50b4a67f98b14d04062a8
Luciano Pastine <lucianopastine.tech@gmail.com>
2021-07-06 20:15:00 -0300

    operadores

---
+++ semana_2/01_javascript_basico/04_operadores/script.js
@@ -0,0 +1,69 @@
+// Operadores aritméticos
+
+// adição +
+var sumTwoNumbers = 1 + 3
+console.log(sumTwoNumbers)
+
+// subtração -
+var subTwoNumbers = 1 - 3
+console.log(subTwoNumbers)
+
+// multiplicação *
+var multTwoNumbers = 1 * 3
+console.log(multTwoNumbers)
+
+// divisão /
+var divTwoNumbers = 1 / 3
+console.log(divTwoNumbers)
+
+// módulo ou resto de uma divisão %
+var restTwoNumbers = 1 & 3
+console.log(restTwoNumbers)
+
+// incremento ++
+var num = 1
+console.log(++num)
+num = num + 1
+num += 1
+
+// decremento --
+var num = 1
+console.log(--num)
+num = num - 1
+num -= 1
+
+// Operadores de atribuição
+
+// atribuição =
+var x = 1
+var y = 3
+
+// atribuição de soma +=
+console.log(x+=y)
+
+// atribuição de subtração -=
+console.log(x-=y)
+
+// atribuição de multiplicação *=
+console.log(x*=y)
+
+// atribuição de divisão /=
+console.log(x/=y)
+
+// atribuição de resto %=
+console.log(x%=y)
+
+// Operadores de comparação
+
+// == para comparar valores
+// === para comparar valor e tipo
+// != diferente de
+// !== diferente de valor e tipo
+// < ou <= menor ou menor-igual
+// > ou >= maior ou maior-igual
+
+// Operadores lógicos
+
+// e &&
+// ou ||
+// negação !
